263665d82ee93ff65f0dee409e318ef60867cd60
이재현 <izg0820@naver.com>
2022-08-04 12:12:27 +0900

    Step5

--- ArmeriaDemo/src/main/java/example/armeria/server/blog/BlogService.java
+++ ArmeriaDemo/src/main/java/example/armeria/server/blog/BlogService.java
@@ -1,13 +1,14 @@
 package example.armeria.server.blog;
 
 import com.linecorp.armeria.common.HttpResponse;
-import com.linecorp.armeria.server.annotation.Post;
+import com.linecorp.armeria.server.annotation.*;
 
 
-import com.linecorp.armeria.server.annotation.RequestConverter;
-
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public final class BlogService {
     private final Map<Integer, BlogPost> blogPosts = new ConcurrentHashMap<>();
@@ -17,5 +18,23 @@ public final class BlogService {
     public HttpResponse createBlogPost(BlogPost blogPost) {
         blogPosts.put(blogPost.getId(), blogPost);
         return HttpResponse.ofJson(blogPost);
+    }
+
+    @Get("/blogs:id") //query parameter or path variable를 사용하기 위해서 :id와 같이 추가한다.
+    public HttpResponse getBlogPost(@Param int id) {
+        BlogPost blogPost = blogPosts.get(id);
+        return HttpResponse.ofJson(blogPost);
+    }
+
+    @Get("/blogs")
+    @ProducesJson //Convert list of object to Json
+    public Iterable<BlogPost> getBlogPosts(@Param @Default("true") boolean descending) {
+        if(descending) {
+            return blogPosts.entrySet()
+                    .stream()
+                    .sorted(Collections.reverseOrder(Comparator.comparingInt(Map.Entry::getKey)))
+                    .map(Map.Entry::getValue).collect(Collectors.toList());
+        }
+        return blogPosts.values().stream().collect(Collectors.toList());
     }
 }
